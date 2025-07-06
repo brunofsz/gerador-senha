d069192a18dfde800c9dec8a4c1bc728f15f4b23
BRUNO FRANCISCO DE SOUZA <141368717+brunofsz@users.noreply.github.com>
2025-07-06 18:33:24 -0300

    Update script.js
    Adicionando a possibilidade de personalização na criação de senhas aleatórias

--- JS/script.js
+++ JS/script.js
@@ -2,6 +2,14 @@
 const btnGerarSenha = document.querySelector("#gerar-senha");
 const elementoGerarSenha = document.querySelector("#gerador-senha");
 
+const abreGerador = document.querySelector("#abrir-gerar-senha");
+const containerGerador = document.querySelector("#gerar-opcoes");
+const lengthInput = document.querySelector("#tamanho");
+const letrasInput = document.querySelector("#letras");
+const numeroInput = document.querySelector("#numeros");
+const simbolosInput = document.querySelector("#simbolos");
+const copy = document.querySelector("#copy");
+
 //FUNCTIONS
 
 const pegaLetraMinuscula = () => {
@@ -28,16 +36,25 @@ const geraSenha = (
   pegaSimbolo
 ) => {
   let senha = "";
-  const senhaLength = 10;
+  const senhaLength = +lengthInput.value;
 
-  const gerador = [
-    pegaLetraMinuscula,
-    pegaLetraMaiuscula,
-    pegaNumero,
-    pegaSimbolo,
-  ];
+  const gerador = [];
 
-  for (i = 0; i < senhaLength; i = i + 4) {
+  if (letrasInput.checked) {
+    gerador.push(pegaLetraMinuscula, pegaLetraMaiuscula);
+  }
+  if (numeroInput.checked) {
+    gerador.push(pegaNumero);
+  }
+  if (simbolosInput.checked) {
+    gerador.push(pegaSimbolo);
+  }
+  if (gerador.length === 0) {
+    alert("Selecione pelo menos uma opção (letras, números ou símbolos)!");
+    return;
+  }
+
+  for (i = 0; i < senhaLength; i = i + gerador.length) {
     gerador.forEach(() => {
       const randomValue = gerador[Math.floor(Math.random() * gerador.length)]();
       senha += randomValue;
@@ -55,4 +72,21 @@ const geraSenha = (
 
 btnGerarSenha.addEventListener("click", (e) => {
   geraSenha(pegaLetraMinuscula, pegaLetraMaiuscula, pegaNumero, pegaSimbolo);
+});
+
+abreGerador.addEventListener("click", (e) => {
+  containerGerador.classList.toggle("hide");
+});
+
+copy.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const senha = elementoGerarSenha.querySelector("h4").innerText;
+
+  navigator.clipboard.writeText(senha).then(() => {
+    copy.innerText = "Copiado!";
+    setTimeout(() => {  
+      copy.innerText = "Copiar";
+    }, 2000);
+  });
 });
